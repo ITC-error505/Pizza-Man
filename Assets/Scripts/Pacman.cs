fdeeefc9f12435f225b76053d81828837b316a8f
bearismybrother <thetwai123456789@gmail.com>
2025-04-25 23:33:41 -0700

    Allowed the direction to change based on holding

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,5 +1,5 @@
-
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Pacman : MonoBehaviour
@@ -15,37 +15,54 @@ public class Pacman : MonoBehaviour
     {
         this.movement = GetComponent<Movement>();
     }
-    
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow  ))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             this.movement.SetDirection(Vector2.up);
         }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             this.movement.SetDirection(Vector2.down);
         }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             this.movement.SetDirection(Vector2.left);
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             this.movement.SetDirection(Vector2.right);
         }
 
-       up.onClick.AddListener(() => this.movement.SetDirection(Vector2.up));
-       down.onClick.AddListener(() => this.movement.SetDirection(Vector2.down));
-       left.onClick.AddListener(() => this.movement.SetDirection(Vector2.left));
-       right.onClick.AddListener(() => this.movement.SetDirection(Vector2.right));
+        //up.onClick.AddListener(() => this.movement.SetDirection(Vector2.up));
+        //down.onClick.AddListener(() => this.movement.SetDirection(Vector2.down));
+        //left.onClick.AddListener(() => this.movement.SetDirection(Vector2.left));
+        //right.onClick.AddListener(() => this.movement.SetDirection(Vector2.right));
 
-       float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
         this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
 
-    } 
+    public void OnUpPressed(BaseEventData data)
+    {
+        this.movement.SetDirection(Vector2.up);
+    }
 
-  
+    public void OnDownPressed(BaseEventData data)
+    {
+        this.movement.SetDirection(Vector2.down);
+    }
+
+    public void OnLeftPressed(BaseEventData data)
+    {
+        this.movement.SetDirection(Vector2.left);
+    }
+
+    public void OnRightPressed(BaseEventData data)
+    {
+        this.movement.SetDirection(Vector2.right);
+    }
 
     public void ResetState()
     {
